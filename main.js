8973452d847916a7ccbcacb5486b22e1215b74d4
Hikari7 <h.kobe712@gmail.com>
2022-10-03 16:57:00 -0700

    added modal window

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+//menu
 const open = document.getElementById("open");
 const overlay = document.querySelector(".overlay");
 const close = document.getElementById("close");
+
+//shopping
+const addBtns = document.querySelectorAll(".cart");
+
+const modal = document.querySelector(".modal");
+const modalBody = document.querySelector(".modal_container");
 
 open.addEventListener("click", () => {
   overlay.classList.add("show");
@@ -10,6 +17,21 @@ close.addEventListener("click", () => {
   overlay.classList.remove("show");
 });
 
+addBtns.forEach((addBtn) => {
+  addBtn.addEventListener("click", () => {
+    modal.classList.add("show_mordal");
+    modalBody.classList.add("show_modal_container");
+  });
+});
 
+//content of the modal
+const goCart = document.querySelector(".go_cart");
+const closes = document.querySelectorAll(".closes");
 
-
+closes.forEach((close) => {
+  close.addEventListener("click", () => {
+    console.log("clicked");
+    modal.classList.remove("show_mordal");
+    modalBody.classList.remove("show_modal_container");
+  });
+});
