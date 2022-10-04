75b5cb6d722debff62c326397f570e53ec61f092
Hikari7 <h.kobe712@gmail.com>
2022-10-03 21:28:01 -0700

    added the modal close btn

--- main.js
+++ main.js
@@ -10,12 +10,15 @@ const modal = document.querySelector(".modal");
 const modalBody = document.querySelector(".modal_container");
 
 open.addEventListener("click", () => {
+  console.log("aaa");
   overlay.classList.add("show");
 });
 
 close.addEventListener("click", () => {
   overlay.classList.remove("show");
 });
+
+//modal
 
 addBtns.forEach((addBtn) => {
   addBtn.addEventListener("click", () => {
@@ -25,13 +28,26 @@ addBtns.forEach((addBtn) => {
 });
 
 //content of the modal
-const goCart = document.querySelector(".go_cart");
 const closes = document.querySelectorAll(".closes");
+const goCart = document.querySelector(".go_cart");
 
 closes.forEach((close) => {
   close.addEventListener("click", () => {
-    console.log("clicked");
+    console.log("close");
     modal.classList.remove("show_mordal");
     modalBody.classList.remove("show_modal_container");
+  });
+});
+
+//Cart
+// const modal = document.querySelector(".modal");
+const seeCarts = document.querySelectorAll(".shopping_cart");
+const cartModal = document.querySelector(".cart_modal");
+const cartBody = document.querySelector(".cart_container");
+
+seeCarts.forEach((seeCart) => {
+  seeCart.addEventListener("click", () => {
+    cartModal.classList.add("show_cart_mordal");
+    cartBody.classList.add("show_cart_container");
   });
 });
